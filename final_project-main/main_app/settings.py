c2d93486c25dd4d261f784916d9b773e1f44c12c
Oana <oana@pinkfox.ro>
2023-02-13 15:52:07 +0200

    add all files for project's example

--- final_project-main/main_app/settings.py
+++ final_project-main/main_app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!=t(fz@z8@c7ysvsm@e&&eefnpa01z3$fd4qpne@d-!3j^&r_q'
+SECRET_KEY = ''
 
 GOOGLE_API_KEY = ""
 BASE_COUNTRY = 'SK'
